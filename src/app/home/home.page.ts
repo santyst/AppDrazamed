f50684f59323a229a4c03a9c377bb564202e01c5
Alejandro Mejia <alejomejia1@gmail.com>
2020-07-29 11:49:44 -0500

    Ultima actualizacion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuController, IonSlides } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -18,18 +20,34 @@ sliderOpts = {
   }
 };
 
-categories = ['AGUAS', 'ANTIAGREGANTE PLAQUETARIO', 'dolor e inflamacion', 'gripa y tos', 'mauricio', 'elbert', 'juan', 'camaro', 'hola', 'maincra',
-'damian', 'xd'];
+public items: any;
+public items2: any;
+public items3: any;
+public items4: any;
+apiURL = `https://drazamed.com/medicine/load-medicine-cats/0`;
+apiURL2 = `https://drazamed.com/favorites`;
 
   constructor(
     private router: Router,
-    private menuCtrl: MenuController
-  ) {}
+    private menuCtrl: MenuController,
+    private http: HttpClient
+  ) {
+    this.http.get(`${this.apiURL}`).subscribe((response) => {
+      this.items = response;
+      this.items2 = this.items [0].result.msg;
+  });
+
+    this.http.get(`${this.apiURL2}`).subscribe((response) => {
+      this.items3 = response;
+      this.items4 = this.items3.result.msg [0].name;
+      console.log(this.items4);
+    });
+
+  }
 
     ionViewWillEnter() {
  this.menuCtrl.enable(true);
 }
-
 
 
 }
