6eb624f605ed9601269e7ece32d9836ef954926b
Alejandro Mejia <alejomejia1@gmail.com>
2020-09-20 20:33:18 -0500

    Cambio de nombres formularios de registro

--- src/app/createaccount2/createaccount2.page.ts
+++ src/app/createaccount2/createaccount2.page.ts
@@ -3,9 +3,10 @@ import { MenuController, AlertController } from '@ionic/angular';
 import { UserService } from '../services/user.service';
 import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import * as moment from 'moment';
 import { uniqueDisplayName } from '../validators/unique_user';
+
 
 
 @Component({
@@ -14,7 +15,8 @@ import { uniqueDisplayName } from '../validators/unique_user';
   styleUrls: ['./createaccount2.page.scss'],
 })
 export class Createaccount2Page implements OnInit {
-
+  data1: any;
+  form: any;
   public registerForm: FormGroup;
   constructor(
     private router: Router,
@@ -22,26 +24,33 @@ export class Createaccount2Page implements OnInit {
     private formBuilder: FormBuilder,
     public http: HttpClient,
     private alertController: AlertController,
-    private userService: UserService
+    private userService: UserService,
+    private route: ActivatedRoute
   ) {
     this.registroForm = this.formBuilder.group({
       documento: new FormControl('', [Validators.required]),
       nacimiento: new FormControl('', [Validators.required, Validators.minLength(9)]),
-      direccion: new FormControl('', [Validators.required]),
-      telefono: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+      address: new FormControl('', [Validators.required]),
+      phone: new FormControl('', [Validators.required, Validators.maxLength(10)]),
       genero: new FormControl('', [Validators.required]),
       convenio: new FormControl('', [Validators.required]),
       aceptar: new FormControl('', [Validators.requiredTrue])
     });
-   }
+    this.route.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.data1 = this.router.getCurrentNavigation().extras.state.user;
+
+      }
+    });
+  }
 
   status: string;
   accept: boolean;
   register = {
     documento: '',
     nacimiento: this.doSomething,
-    direccion: '',
-    telefono: '',
+    address: '',
+    phone: '',
     genero: '',
     convenio: ''
   };
@@ -50,7 +59,7 @@ export class Createaccount2Page implements OnInit {
 
   // tslint:disable-next-line: variable-name
   error_messages = {
-    aceptar: [{type: 'required', message: 'Acepta los términos y condiciones'}]
+    aceptar: [{ type: 'required', message: 'Acepta los términos y condiciones' }]
   };
 
 
@@ -59,15 +68,31 @@ export class Createaccount2Page implements OnInit {
 
   ionViewWillEnter() {
     this.menuCtrl.enable(false);
-   }
-  doSomething(nacimiento){
-     moment(nacimiento).format('YYYY-MM-DD');
   }
-  updateD(){
-    console.log(this.register);
-    /*this.http.post(`https://reqres.in/api/users`, this.register
+  doSomething(nacimiento) {
+    moment(nacimiento).format('YYYY-MM-DD');
+  }
+  async updateD() {
+    const datos = Object.assign(this.data1, this.register);
+    console.log(datos);
+    /*this.http.post(`https://reqres.in/api/users`, datos
     , {headers: new HttpHeaders({'Content-Type': 'application/json'})}).subscribe((mensaje) => {
     console.log(mensaje);
   });*/
+    const alert = await this.alertController.create({
+      header: 'La cuenta ha sido creada',
+      message: 'Verifica tu correo para activar la cuenta.',
+      mode: 'ios',
+      cssClass: 'failed',
+      backdropDismiss: false,
+      buttons: [
+        {
+          text: 'Aceptar',
+          cssClass: 'btnalert',
+          handler: (data) => { this.router.navigate(['login2']); }
+        }
+      ]
+    });
+    alert.present();
   }
 }
