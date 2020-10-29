4837c2556de6025cee31ab3bc6e545246af4b204
Alejandro Mejia <alejomejia1@gmail.com>
2020-10-29 18:58:07 -0500

    Se integra información de usurario en perfil

--- src/app/pages/editprofile/editprofile.page.ts
+++ src/app/pages/editprofile/editprofile.page.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { AuthService } from 'src/app/services/auth.service';
+import { ConfigService } from 'src/app/services/config.service';
 
 @Component({
   selector: 'app-editprofile',
@@ -9,18 +11,39 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class EditprofilePage implements OnInit {
   user: any;
-  constructor(private menuCtrl: MenuController, private auth: AuthService) { }
+  base_url: any;
+  useremail: any;
+  profile: any;
+  firstName: any;
+  lastName: any;
+  mail: any;
+  id: any;
+  address: any;
+  phone: any;
+  constructor(private menuCtrl: MenuController, private auth: AuthService, private config: ConfigService, private http: HttpClient) {
+    this.base_url = config.get_base_url();
+    this.menuCtrl.enable(true);
+    this.user = this.auth.getusuario();
+    this.useremail = this.user.email;
+    this.http.get(`${this.base_url}user/get-user-data/0?email=${this.useremail}`).subscribe((val) => {
+      this.profile = val;
+      this.firstName = this.profile.first_name;
+      this.lastName = this.profile.last_name;
+      this.mail = this.profile.mail;
+      this.id = this.profile.idn;
+      this.address = this.profile.address;
+      this.phone = this.profile.phone;
+      console.log(this.profile);
+    });
+  }
 
   ngOnInit() {
   }
   ionViewWillEnter() {
-    this.menuCtrl.enable(true);
-    this.user = this.auth.getusuario();
- 
-    console.log(this.user);
+
   }
 
   updateProfile() {
-    
+
   }
 }
