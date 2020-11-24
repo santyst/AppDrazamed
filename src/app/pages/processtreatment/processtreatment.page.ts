6ac25a43ee775b87e24d8164864b8b903ed6bb97
Alejandro Mejia <alejomejia1@gmail.com>
2020-11-23 20:52:15 -0500

    Se integra estado del tratamientos con tomas y faltantes

--- src/app/pages/processtreatment/processtreatment.page.ts
+++ src/app/pages/processtreatment/processtreatment.page.ts
@@ -9,21 +9,34 @@ import { ConfigService } from 'src/app/services/config.service';
 })
 export class ProcesstreatmentPage implements OnInit {
 
-  porcentaje = 70;
-  restante = 100 - this.porcentaje;
+  porcentaje: any;
+  restante: any;
   alarma: any;
+  pastillas = 3;
   apiUrl7 = `images/products/`;
   apiUrl8 = `.jpg`;
   base_url: any;
-
+  tomadas: any;
+  faltantes: any;
+  taken: any;
   constructor(private router: Router, private route: ActivatedRoute, private config: ConfigService) {
     this.base_url = config.get_base_url();
     this.route.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
         this.alarma = this.router.getCurrentNavigation().extras.state.user;
         console.log(this.alarma);
+        this.taken = this.alarma.tomadas;
+        this.porcentaje = (this.taken / this.pastillas) * 100;
+        this.restante = Math.ceil(100 - this.porcentaje);
+        this.faltantes = this.pastillas - this.taken;
+        console.log('Se ha tomado ' + this.taken);
       }
     });
+
+    /*this.taken = this.pastillas - 1;
+    console.log(this.taken);
+    this.porcentaje = (this.taken / this.pastillas) * 100;
+    this.restante = 100 - this.porcentaje;*/
   }
 
   ngOnInit() {
